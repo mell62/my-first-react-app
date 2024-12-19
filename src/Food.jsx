18a77d1df3f2051780164200704db53dc51c8737
Mridhul <mridhul67@gmail.com>
2024-12-20 00:28:53 +0530

    Practise adding custom components

---
+++ src/Food.jsx
@@ -0,0 +1,12 @@
+export { FavoriteFood };
+
+function FavoriteFood() {
+  return (
+    <div>
+      <h2>
+        My favorite food is <strong>uranium 235</strong>!
+      </h2>
+      <div>Tons of calories in one small bite!</div>
+    </div>
+  );
+}
