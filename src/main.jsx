908d3df12b30ef5647e2004db5b0c044f383b992
Mridhul <mridhul67@gmail.com>
2024-12-20 01:05:09 +0530

    Add a practise component with SVG and input

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { FavoriteFood } from "./Food.jsx";
 import Greeting from "./Greeting.jsx";
+import { RandomSVG } from "./SVG.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Greeting />
     <FavoriteFood />
+    <RandomSVG />
   </StrictMode>
 );
